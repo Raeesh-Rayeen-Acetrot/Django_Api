316294f46932f7333c5d9f7f9477143c9abaa8d0
Acetrot-Raeesh-Rayeen <raeesh.rayeen@acetrot.com>
2023-02-02 22:58:18 +0530

    updated view file

--- Api/dom/views.py
+++ Api/dom/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def myfun(request):
     return render(request,'index.html')
+
+
+def mydata(request):
+    return render(request,'index.html')
